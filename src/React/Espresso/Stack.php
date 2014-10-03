a3eb9f85b53f13bf1bcc98c0217f55c3d01bf3c4
sderooij <sebassie@fake.com>
2014-10-03 14:26:14 +0200

    upgrade to silex 2

--- src/React/Espresso/Stack.php
+++ src/React/Espresso/Stack.php
@@ -6,24 +6,24 @@ use React\EventLoop\Factory;
 use React\Socket\Server as SocketServer;
 use React\Http\Server as HttpServer;
 
-class Stack extends \Pimple
+class Stack extends \Pimple\Container
 {
     public function __construct($app)
     {
-        $this['loop'] = $this->share(function () {
+        $this['loop'] = function () {
             return Factory::create();
-        });
+        };
 
-        $this['socket'] = $this->share(function ($stack) {
+        $this['socket'] = function ($stack) {
             return new SocketServer($stack['loop']);
-        });
+        };
 
-        $this['http'] = $this->share(function ($stack) {
+        $this['http'] = function ($stack) {
             return new HttpServer($stack['socket']);
-        });
+        };
 
         $isFactory = is_object($app) && method_exists($app, '__invoke');
-        $this['app'] = $isFactory ? $this->protectService($app) : $app;
+        $this['app'] = $isFactory ? $this->protect($app) : $app;
     }
 
     public function listen($port, $host = '127.0.0.1')
@@ -31,13 +31,5 @@ class Stack extends \Pimple
         $this['http']->on('request', $this['app']);
         $this['socket']->listen($port, $host);
         $this['loop']->run();
-    }
-
-    // Pimple::protect minus the type hint
-    public function protectService($callable)
-    {
-        return function ($c) use ($callable) {
-            return $callable;
-        };
     }
 }
