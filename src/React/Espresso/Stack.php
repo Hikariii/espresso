5ce700375c4e2e3a6358f853da7defcf1456e8a2
Hikariii <sderooij.git@gmail.com>
2017-08-01 13:33:45 +0200

    new http implementation used

--- src/React/Espresso/Stack.php
+++ src/React/Espresso/Stack.php
@@ -8,30 +8,32 @@ use React\Http\Server as HttpServer;
 
 class Stack extends \Pimple\Container
 {
-    public function __construct($app, array $values = array())
-    {
-        parent::__construct($values);
+	public function __construct($app, array $values = array())
+	{
+		parent::__construct($values);
 
-        $this['loop'] = function () {
-            return Factory::create();
-        };
+		$this['uri'] = null;
 
-        $this['socket'] = function ($stack) {
-            return new SocketServer($stack['loop']);
-        };
+		$this['loop'] = function () {
+			return Factory::create();
+		};
 
-        $this['http'] = function ($stack) {
-            return new HttpServer($stack['socket']);
-        };
+		$this['socket'] = function ($stack) {
+			return new SocketServer($stack['uri'], $stack['loop']);
+		};
 
-        $isFactory = is_object($app) && method_exists($app, '__invoke');
-        $this['app'] = $isFactory ? $this->protect($app) : $app;
-    }
+		$this['http'] = function ($stack) {
+			return new HttpServer($stack['socket']);
+		};
 
-    public function listen($port, $host = '127.0.0.1')
-    {
-        $this['http']->on('request', $this['app']);
-        $this['socket']->listen($port, $host);
-        $this['loop']->run();
-    }
+		$isFactory = is_object($app) && method_exists($app, '__invoke');
+		$this['app'] = $isFactory ? $this->protect($app) : $app;
+	}
+
+	public function listen($port, $host = '127.0.0.1')
+	{
+		$this['uri'] = $host . ':' . $port;
+		$this['http']->on('request', $this['app']);
+		$this['loop']->run();
+	}
 }
